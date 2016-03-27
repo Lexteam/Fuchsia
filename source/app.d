13dd0263b9e7e8f75107dd01587e03055b1d00e4
Jamie Mansfield <dev@jamierocks.uk>
2016-03-27 02:16:38 +0100

    Lay down the foundatiom
    I would like to point out that it is currently quarter past 2 in the morning and I didn't feel like implementing the whole thing.

--- source/app.d
+++ source/app.d
@@ -4,10 +4,64 @@ void main(string[] args) {
 	if (args.length == 1) {
         writeln("Usage: fushsia <command> [options...]");
         writeln("where the possible options are:");
-        writeln("   -version        Displays the version information.");
+        writeln("   version               Displays the version information.");
+        writeln("   init [license] [lang] Installs everything.");
+        writeln("   license [mit|apache2] Installs the license to the current directory.");
+        writeln("   gitignore [lang]      Installs a gitignore.");
+        writeln("   gitattributes [lang]  Installs a gitattributes.");
     } else {
-        if (args[1] == "-version") {
+        if (args[1] == "version") {
             writeln("fushsia 1.0.0-indev");
+        } else if (args[1] == "init") {
+            string license = "mit";
+            string lang = "java";
+            
+            if (args.length >= 3) {
+                license = args[2];
+            }
+            if (args.length >= 4) {
+                lang = args[3];
+            }
+            
+            installLicense(license);
+            installGitignore(lang);
+            installGitattributes(lang);
+        } else if (args[1] == "license") {
+            string license = "mit";
+            
+            if (args.length >= 3) {
+                license = args[2];
+            }
+            
+            installLicense(license);
+        } else if (args[1] == "gitignore") {
+            string lang = "java";
+            
+            if (args.length >= 3) {
+                lang = args[2];
+            }
+            
+            installGitignore(license);
+        } else if (args[1] == "gitattributes") {
+            string lang = "java";
+            
+            if (args.length >= 3) {
+                lang = args[2];
+            }
+            
+            installGitattributes(license);
         }
     }
+}
+
+void installLicense(string license) {
+    writefln("Installing %s license.", license);
+}
+
+void installGitignore(string lang) {
+    writefln("Installing %s gitignore.", lang);
+}
+
+void installGitattributes(string lang) {
+    writefln("Installing %s gitattributes.", lang);
 }
